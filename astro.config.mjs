09d31333eb53a164a68374283d26ccef1c1dbb79
afara <afara@kiebot.com>
2025-07-29 13:27:20 +0530

    Afara | Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,17 @@
 // export default defineConfig({
 //   base: '/AmigoLIVING/', // ✅ must match your repo name exactly
 // });
+//---------------
+// import { defineConfig } from 'astro/config';
+
+// export default defineConfig({
+//   site: 'https://ladiespgbangalore.in',
+//   base: '/',
+// });
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+
+export default defineConfig({
+  site: 'https://ladiespgbangalore.in', // ✅ Important for correct sitemap URLs
+  integrations: [sitemap()],
+})
